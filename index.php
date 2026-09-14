e8ea4a228d6f3b85b5db1945e5f5e444362781ab
Micael <micael.lima@aluno.senai.br>
2026-09-14 15:28:22 -0300

    atualização : usando php no HTML

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-echo "<h1>Óla Mundo</h1>";
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML no PHP</title>
+</head>
+<body>
+    <?php
 
-echo "<h1>Meu nome:</h1>";
-echo "<p>Micael Álvaro de Freitas Baraneki de Lima</p>";
+
+    echo "<h1>Óla Mundo</h1>";
+    echo "<h1>Óla Mundo</h1>";
+
+    echo "<h1>Meu nome:</h1>";
+    echo "<p>Micael Álvaro de Freitas Baraneki de Lima</p>";
+
+    
+    ?>
+</body>
+</html>
