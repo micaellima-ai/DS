e2c5add9982d8d2863f8158a9b220526e1175dc4
Micael <micael.lima@aluno.senai.br>
2026-09-14 15:29:26 -0300

    atualização : usando php no HTML (arrumado tinha 2 "ola mundo")

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 
 
     echo "<h1>Óla Mundo</h1>";
-    echo "<h1>Óla Mundo</h1>";
+   
 
     echo "<h1>Meu nome:</h1>";
     echo "<p>Micael Álvaro de Freitas Baraneki de Lima</p>";
 
-    
+
     ?>
 </body>
 </html>
