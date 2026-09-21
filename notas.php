519c3c179aca5927e0bd66cdc16676d73712381b
Micael <micael.lima@aluno.senai.br>
2026-09-21 16:05:49 -0300

    Condição ($situação)

--- notas.php
+++ notas.php
@@ -2,6 +2,7 @@
 $nome= "";
 $idade= 0;
 $media = 0;
+$situação = "";
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -23,6 +24,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         ) / 10; 
 }
 
+if ($situação >= 7){
+    $situação = "Aprovado";
+} elseif($situação >= 5 && $situação < 7){
+    $situação = "Recuperação";
+} else {
+    $situação = "Reprovado";
+}
+
 ?>
 
 
@@ -39,39 +48,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
         <label for="idade">Idade</label>
         <input type="number" id="idade" name="idade" placeholder="Digite a idade">
+        <br><br>
 
         <label for="nota1">Nota1</label>
-        <input type="number" name="nota1" id="nota1" placeholder="Digite sua 1° Nota">
+        <input type="number" name="nota1" id="nota1" min = 0 max = 2  placeholder="Digite sua 1° Nota">
         <br><br>
 
         <label for="nota2">Nota2</label>
-        <input type="number" name="nota2" id="nota2" placeholder="Digite sua 2° Nota">
+        <input type="number" name="nota2" id="nota2" min = 0 max = 3 placeholder="Digite sua 2° Nota">
         <br><br>
 
         <label for="nota3">Nota3</label>
-        <input type="number" name="nota3" id="nota3" placeholder="Digite sua 3° Nota">
+        <input type="number" name="nota3" id="nota3" min = 0 max = 1 placeholder="Digite sua 3° Nota">
         <br><br>
 
         <label for="nota4">Nota4</label>
-        <input type="number" name="nota4" id="nota4" placeholder="Digite sua 4° Nota">
+        <input type="number" name="nota4" id="nota4" min = 0 max = 1 placeholder="Digite sua 4° Nota">
         <br><br>
 
         <label for="nota5">Nota5</label>
-        <input type="number" name="nota5" id="nota5" placeholder="Digite sua 5° Nota">
+        <input type="number" name="nota5" id="nota5" min = 0 max = 3 placeholder="Digite sua 5° Nota">
         <br><br>
-        
+
         <button type="submit">Enviar</button>
 
 
+        <?php if ($situação != "" ) { ?>
         <p> Nome Aluno: <?= $nome ?> </p>
         <p> Idade do Aluno: <?= $idade ?> </p>
         <p> Media do Aluno: <?= $media ?> </p>
 
-
-
-
-
-
+        <?php } ?>
 
 <br>
 <a href="index.php">Voltar</a>
