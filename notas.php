bafb0132974154e67a6a5e9d74078aa2a3871b83
Micael <micael.lima@aluno.senai.br>
2026-09-21 17:03:22 -0300

    css

--- notas.php
+++ notas.php
@@ -44,11 +44,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="idade.css">
+    <link rel="stylesheet" href="notas.css">
     <title>Nota Aluno</title>
 </head>
 
 <body>
+    h1
+
     <form method="POST">
 
         <label for="nome">Nome</label>
