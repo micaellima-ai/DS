c86d8cc92e925cf68c1f3b18b87e0cec549a746c
Micael <micael.lima@aluno.senai.br>
2026-09-21 16:50:12 -0300

    Correção GET

--- desafio.php
+++ desafio.php
@@ -7,15 +7,15 @@ $situação = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
-    $nome = $_POST["nome"];
-    $idade = $_POST["idade"];
+    $nome = $_GET["nome"];
+    $idade = $_GET["idade"];
 
 
-    $nota1 = $_POST["nota1"];
-    $nota2 = $_POST["nota2"];
-    $nota3 = $_POST["nota3"];
-    $nota4 = $_POST["nota4"];
-    $nota5 = $_POST["nota5"];
+    $nota1 = $_GET["nota1"];
+    $nota2 = $_GET["nota2"];
+    $nota3 = $_GET["nota3"];
+    $nota4 = $_GET["nota4"];
+    $nota5 = $_GET["nota5"];
 
 
 
