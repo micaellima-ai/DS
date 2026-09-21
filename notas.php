3aef53a18ef98337ea025efeef8173c5e639ded1
Micael <micael.lima@aluno.senai.br>
2026-09-21 16:19:52 -0300

    corrigindo erros

--- notas.php
+++ notas.php
@@ -1,12 +1,12 @@
 <?php
-$nome= "";
-$idade= 0;
+$nome = "";
+$idade = 0;
 $media = 0;
 $situação = "";
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $nome = $_POST["nome"];
     $idade = $_POST["idade"];
 
@@ -20,73 +20,76 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
     $media = (
-        ($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) +($nota4 * 1) + ($nota5 * 3) 
-        ) / 10; 
-}
+        ($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) + ($nota4 * 1) + ($nota5 * 3)
+    ) / 10;
 
-if ($media >= 7){
+
+if ($media >= 7) {
     $situação = "Aprovado";
-} elseif($media >= 5 && $media < 7){
+    } elseif ($media >= 5 && $media < 7) {
     $situação = "Recuperação";
-} else {
-    $situação = "Reprovado";
+    }   else  {
+        $situação = "Reprovado";
+    }
 }
-
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nota Aluno</title>
 </head>
+
 <body>
-        <label for="nome">Nome</label>
-        <input type="text" id="nome" name="nome" placeholder="Digite o nome">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" name="nome" placeholder="Digite o nome">
 
-        <label for="idade">Idade</label>
-        <input type="number" id="idade" name="idade" placeholder="Digite a idade">
-        <br><br>
+    <label for="idade">Idade</label>
+    <input type="number" id="idade" name="idade" placeholder="Digite a idade">
+    <br><br>
 
-        <label for="nota1">Nota1</label>
-        <input type="number" name="nota1" id="nota1" min = 0 max = 10  placeholder="Digite sua 1° Nota">
-        <br><br>
+    <label for="nota1">Nota1</label>
+    <input type="number" name="nota1" id="nota1" min=0 max=10 placeholder="Digite sua 1° Nota">
+    <br><br>
 
-        <label for="nota2">Nota2</label>
-        <input type="number" name="nota2" id="nota2" min = 0 max = 10 placeholder="Digite sua 2° Nota">
-        <br><br>
+    <label for="nota2">Nota2</label>
+    <input type="number" name="nota2" id="nota2" min=0 max=10 placeholder="Digite sua 2° Nota">
+    <br><br>
 
-        <label for="nota3">Nota3</label>
-        <input type="number" name="nota3" id="nota3" min = 0 max = 10 placeholder="Digite sua 3° Nota">
-        <br><br>
+    <label for="nota3">Nota3</label>
+    <input type="number" name="nota3" id="nota3" min=0 max=10 placeholder="Digite sua 3° Nota">
+    <br><br>
 
-        <label for="nota4">Nota4</label>
-        <input type="number" name="nota4" id="nota4" min = 0 max = 10 placeholder="Digite sua 4° Nota">
-        <br><br>
+    <label for="nota4">Nota4</label>
+    <input type="number" name="nota4" id="nota4" min=0 max=10 placeholder="Digite sua 4° Nota">
+    <br><br>
 
-        <label for="nota5">Nota5</label>
-        <input type="number" name="nota5" id="nota5" min = 0 max = 10 placeholder="Digite sua 5° Nota">
-        <br><br>
+    <label for="nota5">Nota5</label>
+    <input type="number" name="nota5" id="nota5" min=0 max=10 placeholder="Digite sua 5° Nota">
+    <br><br>
 
-        <button type="submit">Enviar</button>
+    <button type="submit">Enviar</button>
 
 
-        <?php if ($situação != "" ) { ?>
-        <p> Nome Aluno: <?= $nome ?> </p>
+    <?php if ($situação != "") { ?>
+        <p> Nome do Aluno: <?= $nome ?> </p>
         <p> Idade do Aluno: <?= $idade ?> </p>
         <p>Nota 1: <?= $nota1 ?> </p>
         <p>Nota 2: <?= $nota2 ?> </p>
         <p>Nota 3: <?= $nota3 ?> </p>
         <p>Nota 4: <?= $nota4 ?> </p>
-        <p>Nota 5 <?= $nota5 ?> </p>
+        <p>Nota 5: <?= $nota5 ?> </p>
         <p> Media do Aluno: <?= $media ?> </p>
         <p>Situação: <?= $situação ?> </p>
 
-        <?php } ?>
+    <?php } ?>
 
-<br>
-<a href="index.php">Voltar</a>
+    <br>
+    <a href="index.php">Voltar</a>
 </body>
+
 </html>
