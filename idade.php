cbfc5f4475a582e63a6e947bf26edc9b19dc91e3
Micael <micael.lima@aluno.senai.br>
2026-09-22 16:57:30 -0300

    stilizando

--- idade.php
+++ idade.php
@@ -1,65 +1,67 @@
 <?php
-    $nome = "";
-    $idade = 0;
-    $resultado = "";
+$nome = "";
+$idade = 0;
+$resultado = "";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        $idade = $_POST["idade"];
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+}
 
-    if ($idade > 18) {
-        $resultado = "Sim";
-    } else {
-        $resultado = "Não";
-    }
-    ?>
-    <!DOCTYPE html>
-    <html lang="pt-BR">
+if ($idade >= 18) {
+    $resultado = "Sim";
+} else {
+    $resultado = "Não";
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-        <title>HTML no PHP</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>HTML no PHP</title>
+</head>
 
-    <body>
+<body>
 
-        <div class="ti">
-            <h1>Nome: <?= $nome ?> </h1>
-            <p>Idade: <?= $idade ?></p>
+    <div class="ti">
+        <h1>Nome: <?= $nome ?> </h1>
+        <p>Idade: <?= $idade ?></p>
+    </div>
+
+
+
+    <form method="POST">
+
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome" placeholder="Digite o nome">
+
+        <label for="idade">Idade</label>
+        <input type="number" id="idade" name="idade" placeholder="Digite a idade">
+
+        <button type="submit">Enviar</button>
+
+    </form>
+
+    <?php if ($resultado != "") { ?>
+        <div class="res">
+            <p>
+                Seu nome é <?= $nome ?> e tem <?= $idade ?> anos.
+                <br>
+                Você é de maior de idade?: <?= $resultado ?>
+            </p>
         </div>
 
+        </div>
+    <?php } ?>
+
+    <br><br>
+
+    <a href="index.php">Voltar</a>
 
 
-        <form method="POST">
+</body>
 
-            <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome" placeholder="Digite o nome">
-
-            <label for="idade">Idade</label>
-            <input type="number" id="idade" name="idade" placeholder="Digite a idade">
-
-            <button type="submit">Enviar</button>
-
-        </form>
-
-        <?php if ($resultado != "") { ?>
-            <div class="res">
-                <p > Seu nome é <?= $nome ?> e tem <?= $idade ?> anos.
-                    <br>
-                    Você é de maior de idade?: <?= $resultado ?></p> 
-                </p>
-
-            </div>
-        <?php } ?>
-
-        <br><br>
-
-        <a href="index.php">Voltar</a>
-
-
-    </body>
-
-    </html>
+</html>
