37fbaf8a932a939448d55e48e46ee4da6b56270e
Micael <micael.lima@aluno.senai.br>
2026-09-22 17:04:45 -0300

    Update desafio.php

--- desafio.php
+++ desafio.php
@@ -45,7 +45,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="notas.css">
-    <title>Nota Aluno</title>
+
+    <title>Nota (Desafio)</title>
 </head>
 
 <body>
